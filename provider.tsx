2d308d2c0acc925e3911e5afe9af81024ace81b2
0xanonymeow <anonymeow@incatni.to>
2023-06-07 14:42:33 +0700

    build: add provider

---
+++ provider.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+import { Toaster } from 'react-hot-toast';
+
+export const Providers = ({ children }: { children?: ReactNode }) => {
+  return (
+    <>
+      <Toaster />
+      {children}
+    </>
+  );
+};
